7bf8895bc68700bef2e9e8135a61cf47e999c291
thiagoaraujocampos <thiagoaraujocampos@outlook.com>
2019-12-04 12:33:52 -0300

    tp05

--- tp05/exemplo.c
+++ tp05/exemplo.c
@@ -1,100 +1,124 @@
+/* C/C++ program to solve N Queen Problem using 
+backtracking */
+#include <stdbool.h> 
 #include <stdio.h>
 #include <stdlib.h>
- 
-void Troca(int v[], int i, int j) {
-  int tmp;
-  tmp = v[i]; 
-  v[i] = v[j]; 
-  v[j] = tmp;
-}
+#define N 10
 
-int SolucaoValida(int linhas[]){
-  int i;
-  int x,y;
-  int xx,yy;
- 
-  for(i = 0; i < 8; i++){
-    x = i;
-    y = linhas[i];
- 
-    xx = x;
-    yy = y;
-    while(1){
-      xx += 1;
-      yy -= 1;
-      if(xx > 7 || yy < 0) break;
-       
-      if(yy == linhas[xx]) 
-        return 0;
-    }
- 
-    xx = x;
-    yy = y;
-    while(1){
-      xx -= 1;
-      yy -= 1;
-      if(xx < 0 || yy < 0) break;
-       
-      if(yy == linhas[xx]) 
-        return 0;
-    }
-  }
-  return 1;
-}
+int cont =  0;
 
-void ImprimeSolucao(int linhas[]){
-  char tabuleiro[8][8];
-  int i,j;
-  int x,y;
-  static int nsols = 0;
- 
-  nsols++;
- 
-  printf("******** SOL: %d ********\n",nsols);
- 
-  for(i = 0; i < 8; i++)
-    for(j = 0; j < 8; j++)
-      tabuleiro[i][j] = '.';
- 
-  for(i = 0; i < 8; i++){
-    x = i;
-    y = linhas[i];
-    tabuleiro[y][x] = 'R';
-  }
- 
-  for(i = 0; i < 8; i++){
-    for(j = 0; j < 8; j++){
-      printf(" %c ",tabuleiro[i][j]);
-    }    
-    printf("\n");
-  }
-}
-
-void TestaPermutacoes(int linhas[], int k) {
-  int i;
-   
-  if(k == 8) {
-    if(SolucaoValida(linhas))
-      ImprimeSolucao(linhas);
-  }
-  else{
-    for(i = k; i < 8; i++) {
-      Troca(linhas, k, i);
-      TestaPermutacoes(linhas, k + 1);
-      Troca(linhas, i, k);
-    }
-  }
+/* A utility function to print solution */
+void printSolution(int board[N][N]) 
+{ 
+	static int k = 1; 
+	printf("%d-\n",k++); 
+	for (int i = 0; i < N; i++) 
+	{ 
+		for (int j = 0; j < N; j++) 
+			printf(" %d ", board[i][j]); 
+		printf("\n"); 
+	} 
+	printf("\n"); 
 } 
 
-void Solucoes8Rainhas() {
-  int linhas[8];
-  int i;
-  for(i = 0; i < 8; i++)
-    linhas[i] = i;
-  TestaPermutacoes(linhas, 0);
-}
- 
-int main(){
-  Solucoes8Rainhas();
-  return 0;
-}
+/* A utility function to check if a queen can 
+be placed on board[row][col]. Note that this 
+function is called when "col" queens are 
+already placed in columns from 0 to col -1. 
+So we need to check only left side for 
+attacking queens */
+bool isSafe(int board[N][N], int row, int col) 
+{ 
+	int i, j; 
+
+	/* Check this row on left side */
+	for (i = 0; i < col; i++) 
+		if (board[row][i]) 
+			return false; 
+
+	/* Check upper diagonal on left side */
+	for (i=row, j=col; i>=0 && j>=0; i--, j--) 
+		if (board[i][j]) 
+			return false; 
+
+	/* Check lower diagonal on left side */
+	for (i=row, j=col; j>=0 && i<N; i++, j--) 
+		if (board[i][j]) 
+			return false; 
+
+	return true; 
+} 
+
+/* A recursive utility function to solve N 
+Queen problem */
+bool solveNQUtil(int board[N][N], int col) 
+{ 
+	/* base case: If all queens are placed 
+	then return true */
+	if (col == N) 
+	{ 
+		cont++;
+		return true; 
+	} 
+
+	/* Consider this column and try placing 
+	this queen in all rows one by one */
+	bool res = false; 
+	for (int i = 0; i < N; i++) 
+	{ 
+		/* Check if queen can be placed on 
+		board[i][col] */
+		if ( isSafe(board, i, col) ) 
+		{ 
+			/* Place this queen in board[i][col] */
+			board[i][col] = 1; 
+
+			// Make result true if any placement 
+			// is possible 
+			res = solveNQUtil(board, col + 1) || res; 
+
+			/* If placing queen in board[i][col] 
+			doesn't lead to a solution, then 
+			remove queen from board[i][col] */
+			board[i][col] = 0; // BACKTRACK 
+		} 
+	} 
+
+	/* If queen can not be place in any row in 
+		this column col then return false */
+	return res; 
+} 
+
+/* This function solves the N Queen problem using 
+Backtracking. It mainly uses solveNQUtil() to 
+solve the problem. It returns false if queens 
+cannot be placed, otherwise return true and 
+prints placement of queens in the form of 1s. 
+Please note that there may be more than one 
+solutions, this function prints one of the 
+feasible solutions.*/
+void solveNQ() 
+{ 
+	int board[N][N]; 
+	for(int i = 0; i < N; i++) {
+    for(int j = 0; j < N; j++) {
+      board[i][j] = 0;
+    }
+  }
+
+	if (solveNQUtil(board, 0) == false) 
+	{ 
+		printf("Solution does not exist"); 
+		return ; 
+	} 
+
+	return ; 
+} 
+
+// driver program to test above function 
+int main() 
+{ 
+	solveNQ(); 
+  printf("%d\n", cont);
+	return 0; 
+} 
